14aaf6a732df65fda3968fd1abbb9d92147e3e62
yahor.kazitski <yahor.kazitski@sap.com>
2022-03-23 23:25:28 +0400

    replace JSON lib

--- src/main/java/com/yk/protfolio/springportfolio/services/ContactService.java
+++ src/main/java/com/yk/protfolio/springportfolio/services/ContactService.java
@@ -2,9 +2,8 @@ package com.yk.protfolio.springportfolio.services;
 
 import com.yk.protfolio.springportfolio.schema.Contact;
 import com.yk.protfolio.springportfolio.utilities.OperationResult;
-import org.hibernate.validator.internal.util.Contracts;
-import org.springframework.boot.configurationprocessor.json.JSONException;
-import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public interface ContactService {
     OperationResult<Contact> postContact(JSONObject contact) throws JSONException;

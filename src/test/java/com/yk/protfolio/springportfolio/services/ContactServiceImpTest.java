03bbcac079f890b199d7bd47245866da04f47c58
yahor.kazitski <yahor.kazitski@sap.com>
2022-04-02 01:35:03 +0400

    .

---
+++ src/test/java/com/yk/protfolio/springportfolio/services/ContactServiceImpTest.java
@@ -0,0 +1,12 @@
+package com.yk.protfolio.springportfolio.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContactServiceImpTest {
+
+    @Test
+    void postContact() {
+    }
+}
---
+++ src/test/java/com/yk/protfolio/springportfolio/services/ImageManagerImpTest.java
@@ -0,0 +1,6 @@
+package com.yk.protfolio.springportfolio.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ImageManagerImpTest {
+  
+}

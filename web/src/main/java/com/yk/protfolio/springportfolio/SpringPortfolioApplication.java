d540fcb2723fb72b4ddaf9e76e852cac40ac5a5b
yahor.kazitski <yahor.kazitski@sap.com>
2022-08-08 23:56:58 +0400

    docker + kafka improvemenets

--- web/src/main/java/com/yk/protfolio/springportfolio/SpringPortfolioApplication.java
+++ web/src/main/java/com/yk/protfolio/springportfolio/SpringPortfolioApplication.java
@@ -3,10 +3,12 @@ package com.yk.protfolio.springportfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"com.yk.protfolio.springportfolio","com.yk.processor","com.yk.dao"})
+@SpringBootApplication(exclude = {KafkaAutoConfiguration.class},
+        scanBasePackages = {"com.yk.protfolio.springportfolio", "com.yk.processor", "com.yk.dao"})
 @EnableConfigurationProperties
 @EntityScan(basePackages = {"com.yk.schema"})
 @EnableScheduling
